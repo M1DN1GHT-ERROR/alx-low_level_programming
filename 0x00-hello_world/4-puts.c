952d27286e0698ca2fd82675840a19c0ca387774
M1DN1GHT-ERROR <ilagsoun021@gmail.com>
2023-03-16 08:11:52 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
- int main(void) 
-{
+  
 /**
  * main - Entry point
  * 
@@ -8,6 +7,8 @@
  *
  * Return: Always 0 (Success)
  **/	
-	puts("\"Programming is like building a multilingual puzzle");
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
 	return 0;
 }
