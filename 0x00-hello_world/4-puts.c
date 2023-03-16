4f61a3ce77877175caa39f23b23e3fdc131640a8
M1DN1GHT-ERROR <ilagsoun021@gmail.com>
2023-03-16 08:04:39 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
  int main(void) 
 {
-	puts(""Programming is like building a multilingual puzzle");
+/**
+ * main - point
+ * return : always 0 (success)
+ **/	
+	puts("\"Programming is like building a multilingual puzzle");
 	return 0;
 }
