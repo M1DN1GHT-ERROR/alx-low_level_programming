1d38f8cff561378007f8b893e21533e09763be62
M1DN1GHT-ERROR <ilagsoun021@gmail.com>
2023-03-16 15:17:16 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 
 /**
- *main - entry point
+ * main - Entry point
  *
- * return: always 0 (succesful)
- */
+ * Description: using sizeof to print the size of various types.
+ *
+ * Return: Always 0 (Succesful)
+*/
 
 int main(void)
 {
+
 	/* sizeof evaluates the size of a variable */
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
 	printf("Size of an int: %lu byte(s)\n", sizeof(int));
@@ -15,5 +18,7 @@ int main(void)
 	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %lu byte(s)\n", sizeof(float));
+
 	return (0);
+
 }
