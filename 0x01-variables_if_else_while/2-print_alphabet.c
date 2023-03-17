20516e537d20dd389cc8ab9b994f1787dad111f8
M1DN1GHT-ERROR <ilagsoun021@gmail.com>
2023-03-17 07:31:56 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase,
+ *
+ * Return: 0 (success)
+ *
+ */
+
+int main(void)
+{
+	ch letter;
+	for (letter = 'a'; letter <= 'z'; ch++)
+	{
+		putchar(letter);
+	}
+	putchar('\n')
+		return (0);
+}
