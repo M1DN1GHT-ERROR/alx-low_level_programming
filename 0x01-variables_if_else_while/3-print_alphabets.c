f47849aa4658cab9c9cb62829c2cb0f193298261
M1DN1GHT-ERROR <ilagsoun021@gmail.com>
2023-03-17 07:45:43 -0700

    uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio>
+
+/**
+ * main - write lower and uppercase alph
+ *
+ * Return: 0 (success)
+ *
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
