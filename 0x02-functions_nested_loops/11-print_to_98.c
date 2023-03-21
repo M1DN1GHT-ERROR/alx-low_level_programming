3dff97fb16932cf52aab9ce21f720aa1f5241af7
M1DN1GHT-ERROR <ilagsoun021@gmail.com>
2023-03-21 16:05:15 -0700

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,27 +1,23 @@
-#include "main.h"
+#include"main.h"
 
 /**
- * print_to_98 - a function that prints all natural numbers from n to 98.
- * @n: The first number should be printed.
- */
+ * print_to_98 - print n to 98 counts
+ *           separated by comma, followed
+ *           by space and number should be
+ *           printed in order
+ *
+ * @n: input
+*/
 
 void print_to_98(int n)
 {
-	int i;
+	int count;
 
-	if (n <= 98)
-	{
-		for (i = n; i < 98; i++)
-		{
-			printf("%d, ", i);
-		}
-	}
+	if (n > 98)
+		for (count = n; count > 98; --count)
+			printf("%d, ", count);
 	else
-	{
-		for (i = n; i > 98; i--)
-		{
-			printf("%d, ", i);
-		}
-	}
+		for (count = n; count < 98; ++count)
+			printf("%d, ", count);
 	printf("98\n");
-}
+
