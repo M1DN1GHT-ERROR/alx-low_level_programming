731fceeceb81aecf0481e6a85e29964ae7493cf0
M1DN1GHT-ERROR <ilagsoun021@gmail.com>
2023-03-21 12:51:05 -0700

    There are only 3 colors, 10 digits, and 7 notes

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,15 +1,22 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * print_last_digit - Prints the last digit of a number
- * @num: The number to process
- *
- * Return: The value of the last digit
+ * print_last_digit - a function that prints the last digit of a number
+ * @nld: number's last digit result
+ * Return: value of the last digit
  */
-int print_last_digit(int num)
-{
-	int last_digit = abs(num % 10);
 
-	_putchar(last_digit + '0');
-	return (last_digit);
+int print_last_digit(int nld)
+{
+	int nb;
+
+	nb = nld % 10;
+
+	if (nb < 0)
+	{
+		nb = (-1 * nb);
+	}
+	_putchar(nb + '0');
+
+	return (nb);
 }
