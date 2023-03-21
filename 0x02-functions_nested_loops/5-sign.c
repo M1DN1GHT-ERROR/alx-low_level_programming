75a5cb70d5092d31c26926e8d19a54f0363efc87
M1DN1GHT-ERROR <ilagsoun021@gmail.com>
2023-03-21 10:16:46 -0700

    siiiign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,17 +10,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		putchar('+');
+		_putchar('+');
 		return (1);
 	}
 	if (n < 0)
 	{
-		putchar('-');
+		_putchar('-');
 		return (-1);
 	}
 	else
 	{
-		putchar('0');
+		_putchar('0');
 		return (0);
 	}
 }
