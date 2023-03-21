84dad4dcca2c17f19c9a9cea81a06c6708187c90
M1DN1GHT-ERROR <ilagsoun021@gmail.com>
2023-03-21 06:32:56 -0700

    0.putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c = "_putchar";
+
+	while (c)
+	{
+		_putchar(c);
+		str++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
