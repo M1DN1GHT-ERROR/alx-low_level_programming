18f7e1097c4c06ea5e7da89ed592d63be7443992
M1DN1GHT-ERROR <ilagsoun021@gmail.com>
2023-03-22 10:40:38 -0700

    0 > 972?

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,23 +12,17 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && b > c)
+	if (a >= b && a >= c)
 	{
-	largest = a;
-	}
-	else if (a > c && c > b)
 		largest = a;
-	else if (b > a && a > c)
-	{
-	largest = b;
 	}
-	else if (b > c && c > a)
+	else if (b >= a && b >= c)
+	{
 		largest = b;
-	else if (c > a && a > b)
-		largest = c;
+	}
 	else
 	{
-	largest = c;
+		largest = c;
 	}
 
 	return (largest);
