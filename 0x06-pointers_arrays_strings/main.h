60f5382c208d5434497ec1bf34610d14b025e6a8
M1DN1GHT-ERROR <ilagsoun021@gmail.com>
2023-03-30 07:53:23 -0700

    It is the addition of strangeness to beauty that constitutes the romantic character in art

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -10,7 +10,7 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
-
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 
 
 #endif
