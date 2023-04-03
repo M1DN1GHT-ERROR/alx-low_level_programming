b116fd9c8df5b81fc99e9f4fbe4a1869d4de989d
M1DN1GHT-ERROR <ilagsoun021@gmail.com>
2023-04-03 14:55:16 -0700

    strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -9,20 +9,22 @@
  * Return: A pointer to the byte in `s` that matches one of the bytes in `accept`,
  *         or NULL if no such byte is found.
  */
-char *_strpbrk(char *s, char *accept)
+char	*_strpbrk(char *s, char *accept)
 {
-    int i, j;
+	int	i;
+	int	j;
 
-    for (i = 0; s[i] != '\0'; i++)
-    {
-        for (j = 0; accept[j] != '\0'; j++)
-        {
-            if (s[i] == accept[j])
-            {
-                return (&s[i]);
-            }
-        }
-    }
-
-    return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		j = 0;
+		while (accept[j])
+		{
+			if (s[i] == accept[j])
+				return (s + i);
+			j++;
+		}
+		i++;
+	}
+	return (NULL);
 }
