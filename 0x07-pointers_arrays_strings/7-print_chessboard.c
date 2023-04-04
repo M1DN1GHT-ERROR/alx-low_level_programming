cf854d564b3800f91cd7aa7752197ec089750b8c
M1DN1GHT-ERROR <ilagsoun021@gmail.com>
2023-04-03 18:20:42 -0700

    Chess is mental torture

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_chessboard - Prints a chessboard
+ *
+ * @a: The chessboard to print, as a 2D array of characters.
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+	for (j = 0; j < 8; j++)
+	{
+		_putchar(a[i][j]);
+		_putchar(' ');
+	}
+	_putchar('\n');
+	}
+}
+
