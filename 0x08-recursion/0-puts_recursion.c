2da38f808f0678e12835ff85eb863d6f6516f4c2
M1DN1GHT-ERROR <ilagsoun021@gmail.com>
2023-04-05 11:23:23 -0700

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * _puts_recursion - prints a string using recursion
+ * @s: pointer to the string to be printed
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+	putchar('\n');
+	return;
+	}
+
+	putchar(*s);
+	_puts_recursion(s + 1);
+}
