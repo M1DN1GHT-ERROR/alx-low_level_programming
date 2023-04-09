8c5577cd497be4ed08cbbf43a6f46ca2370c6849
M1DN1GHT-ERROR <ilagsoun021@gmail.com>
2023-04-09 09:09:42 -0700

    ilence is argument carried out by other means

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the number of arguments passed into it
+ * @argc: the number of arguments passed
+ * @argv: the array of arguments
+ * Return: 0
+ */
+
+int main(int argc, char __attribute__((__unused__))**argv)
+{
+	printf("%d\n", argc);
+	return (0);
+}
