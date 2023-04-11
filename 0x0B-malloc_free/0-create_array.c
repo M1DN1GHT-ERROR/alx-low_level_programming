efd19b90bc50ca9c5747d119c64f10d49b0fe9f0
M1DN1GHT-ERROR <ilagsoun021@gmail.com>
2023-04-11 10:36:47 -0700

    Float like a butterfly, sting like a bee

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - creates an array of chars
+ * @size: size of array
+ * @c: specific char
+ * Return: Returns pointer to the array, NULL if fail, NULL if size is 0
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *arr;
+	unsigned int i;
+
+	arr = malloc(sizeof(char) * size);
+
+	if (size == 0 || arr == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		arr[i] = c;
+	return (arr);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,6 @@
+#0x0B. C - malloc, free#
+malloc is a function that is used to dynamically allocate memory during runtime. It stands for "memory allocation", and is typically used in C and C++ programming languages.
+
+The malloc function is used to request a block of memory from the operating system's heap memory, which is then used to store data for the program. The function takes a single argument, which is the number of bytes of memory to allocate.
+
+
