51778d599259dd8472df412fc496916a4833ae5a
M1DN1GHT-ERROR <ilagsoun021@gmail.com>
2023-04-17 10:52:40 -0700

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,25 @@
+#ifndef DOG_H
+#define DOG_H
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct dog - Details of a dog.
+ * @name: first member
+ * @age: second member
+ * @owner: thid member
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+
+
+#endif
