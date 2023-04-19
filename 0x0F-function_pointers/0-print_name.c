63a5ff3fe3ded1064fb548309417ab447e341ce6
M1DN1GHT-ERROR <ilagsoun021@gmail.com>
2023-04-19 08:33:33 -0700

    What's my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: name to print
+ * @f: pointer to function
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+	{
+		return;
+	}
+	else
+	{
+		f(name);
+	}
+}
