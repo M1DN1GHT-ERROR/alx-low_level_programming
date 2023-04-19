ab074e05c36fc3a6929a6124117aaea23b553d7f
M1DN1GHT-ERROR <ilagsoun021@gmail.com>
2023-04-19 09:24:59 -0700

    If you spend too much time thinking about a thing, you'll never get it done

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,11 +10,14 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
+	unsigned int i;
+
 	if (array == NULL || action == NULL)
 	{
 		return;
 	}
-	for (size_t i = 0; i < size; i++)
+
+	for (i = 0; i < size; i++)
 	{
 	action(array[i]);
 	}
