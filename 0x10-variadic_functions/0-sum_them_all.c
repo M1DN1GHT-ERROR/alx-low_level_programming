385b7ace2fe4647bb0bcc34ca463c1306ee43233
M1DN1GHT-ERROR <ilagsoun021@gmail.com>
2023-04-20 09:41:34 -0700

    Beauty is variable, ugliness is constant

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,24 +8,21 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-	
 	int result = 0;
 	unsigned int i;
 	va_list args;
 
-	if(n == 0)
+	if (n == 0)
 	{
-	return 0;
+	return (0);
 	}
-	
-	
 	va_start(args, n);
+	for (i = 0; i < n; i++)
 
-	for(i = 0; i < n; i++)
-    {
-	    result += va_arg(args, int);
-    }
+	{
+		result += va_arg(args, int);
+	}
 	va_end(args);
 
-	return result;
+	return (result);
 }
