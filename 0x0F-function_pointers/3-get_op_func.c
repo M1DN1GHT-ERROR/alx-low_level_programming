9fbbf7f416d5a26da49530d4e0836557bd198d3f
M1DN1GHT-ERROR <ilagsoun021@gmail.com>
2023-04-19 18:47:32 -0700

    A goal is not always meant to be reached, it often serves simply as something to aim at

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -23,9 +23,14 @@ op_t ops[] = {
 
 	int i = 0;
 
-	while (ops[i].op != NULL && *(ops[i].op) != *s)
+	while (i < 5)
+	{
+		if (strcmp(ops[i].op, s) == 0)
+		{
+			return (ops[i].f);
+		}
 		i++;
+	}
 
-	return (ops[i].f);
+	return (NULL);
 }
-
