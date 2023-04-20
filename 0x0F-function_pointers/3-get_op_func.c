0f745a7d6a6a6417446ef6c96d657d28228b6ccc
M1DN1GHT-ERROR <ilagsoun021@gmail.com>
2023-04-19 18:15:17 -0700

    A goal is not always meant to be reached, it often serves simply as something to aim at

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,35 @@
+#include "calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *get_op_func - Matches function with correct operation
+ *@s: operation sign
+ *Return: to the function that corresponds to the operation given
+ *
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+op_t ops[] = {
+	{"+", op_add},
+	{"-", op_sub},
+	{"*", op_mul},
+	{"/", op_div},
+	{"%", op_mod},
+	{NULL, NULL}
+	};
+	int i = 0;
+
+	while (i < 5)
+	{
+		if (strcmp(ops[i].op, s) == 0)
+		{
+			return (ops[i].f);
+		}
+		i++;
+	}
+
+	return (NULL);
+}
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,74 @@
+#include "calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * op_add - add two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: sum of two numbers
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - substract two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: diffrence of two numbers
+ */
+
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+
+/**
+ * op_mul - product of two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: returns the product of a and b
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divide two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: returns the result of the division of a by b
+ */
+
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+	printf("Error\n");
+	exit(100);
+	}
+	return (a / b);
+}
+
+
+/**
+ * op_mod - modulo of two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: returns the remainder of the division of a by b
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
