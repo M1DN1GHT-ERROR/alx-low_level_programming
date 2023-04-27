4ca6c27f11aad58a61dcbcbdfe391d30c233b0ac
midnight-error <ilagsoun021@gmail.com>
2023-04-27 07:27:54 -0700

    List length

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -25,5 +25,9 @@ typedef struct list_s
 
 
 size_t print_list(const list_t *);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+size_t list_len(const list_t *h);
 
 #endif
