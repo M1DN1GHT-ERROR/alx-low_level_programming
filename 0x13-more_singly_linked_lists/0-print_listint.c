d6c337ff5e4f6a71396622c04c5e32487db8efa8
midnight-error <ilagsoun021@gmail.com>
2023-04-28 07:34:37 -0700

    Print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,14 +10,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t count = 0;
+	size_t i = 0;
 
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
-		count++;
+		i++;
 		h = h->next;
 	}
 
-	return count;
+	return (i);
 }
