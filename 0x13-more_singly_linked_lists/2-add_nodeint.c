0506a11ea44a1c7f8930fb59af5e69991be6d4af
midnight-error <ilagsoun021@gmail.com>
2023-04-29 07:47:02 -0700

    Add node

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -13,9 +13,9 @@ listint_t *add_nodeint(listint_t **head, const int n)
 
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
-    {
-	    return (NULL);
-    }
+	{
+		return (NULL);
+	}
 	new_node->n = n;
 	new_node->next = *head;
 	*head = new_node;
