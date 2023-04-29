7951714244aa2aa54962a52fc6178a04881eeb5d
midnight-error <ilagsoun021@gmail.com>
2023-04-29 06:41:35 -0700

    Print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 
 /**
- * print_listint - prints all the elements of a listint_t list
- * @h: pointer to head of list
+ * print_listint - Prints all the elements of a listint_t list.
+ * @h: Pointer to the head node of the list.
  *
- * Return: the number of nodes
+ * Return: The number of nodes in the list.
  */
 size_t print_listint(const listint_t *h)
 {
@@ -12,9 +12,9 @@ size_t print_listint(const listint_t *h)
 
 	while (h != NULL)
 	{
-		printf("%d\n", h->n);
-		i++;
-		h = h->next;
+	printf("%d\n", h->n);
+	i++;
+	h = h->next;
 	}
 
 	return (i);
