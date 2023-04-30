06355e0831b016c2f8ed2c2302a5a60ddbaad4ed
midnight-error <ilagsoun021@gmail.com>
2023-04-30 15:49:59 -0700

    Reverse list

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -6,24 +6,25 @@
  *
  * Return: number of nodes in the list
  */
+
 size_t print_listint_safe(const listint_t *head)
 {
-    size_t num = 0;
-    long int diff;
+	size_t num = 0;
+	long int diff;
 
-    while (head)
-    {
-        diff = head - head->next;
-        num++;
-        printf("[%p] %d\n", (void *)head, head->n);
-        if (diff > 0)
-            head = head->next;
-        else
-        {
-            printf("-> [%p] %d\n", (void *)head->next, head->next->n);
-            break;
-        }
-    }
+	while (head)
+	{
+		diff = head - head->next;
+		num++;
+		printf("[%p] %d\n", (void *)head, head->n);
+	if (diff > 0)
+		head = head->next;
+	else
+	{
+	printf("-> [%p] %d\n", (void *)head->next, head->next->n);
+	break;
+	}
+	}
 
-    return (num);
+	return (num);
 }
