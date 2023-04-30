2079fcb33dfd6d67c04203aaa1fdef63a74aa87a
midnight-error <ilagsoun021@gmail.com>
2023-04-30 07:07:14 -0700

    Insert

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,48 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - Inserts a new node at a given position
+ * @head: Pointer to pointer to the head node of the list
+ * @idx: Index of the list where the new node should be added
+ * @n: Value of the new node to be added
+ *
+ * Return: Address of the new node, or NULL if it failed
+ */
+
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *new_node, *current_node;
+	unsigned int i;
+
+	if (head == NULL)
+	return (NULL);
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+	return (NULL);
+
+	new_node->n = n;
+	if (idx == 0)
+	{
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
+	}
+
+	current_node = *head;
+	for (i = 0; i < idx - 1; i++)
+	{
+	if (current_node == NULL || current_node->next == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	current_node = current_node->next;
+	}
+
+	new_node->next = current_node->next;
+	current_node->next = new_node;
+
+	return (new_node);
+}
